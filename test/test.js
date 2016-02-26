fbdcaf2cd75d69238edd8e676ef3222a79ddb226
kgryte <kgryte@gmail.com>
2016-02-26 02:34:19 -0800

    Fix test file being in wrong directory

--- test/test.js
+++ test/test.js
@@ -6,6 +6,20 @@ var tape = require( 'tape' );
 var createElement = require( './../lib' );
 
 
+// SETUP //
+
+function setup() {
+	return {
+		'className': 'equation',
+		'align': 'center',
+		'equation': 'y = mx + b',
+		'label': 'eq:line',
+		'src': 'https://cdn.rawgit.com/math-io/repo/branch/docs/img/eqn.svg',
+		'alt': 'Equation for a line.'
+	};
+}
+
+
 // TESTS //
 
 tape( 'main export is a function', function test( t ) {
